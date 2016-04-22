b256825a2f855617216a5f1b2308a274bfd553cb
RobertStreng <robert.streng@mainsoft.de>
2016-04-22 09:16:59 +0200

    Code formatted

--- org.junit.tools/src/org/junit/tools/ui/generator/wizards/MockGeneratorWizard.java
+++ org.junit.tools/src/org/junit/tools/ui/generator/wizards/MockGeneratorWizard.java
@@ -31,182 +31,184 @@ import org.junit.tools.ui.generator.wizards.pages.MockGeneratorWizardPage;
 
 @SuppressWarnings("restriction")
 public class MockGeneratorWizard extends Wizard implements INewWizard,
-	IMethodeSelectionChangedListener {
+		IMethodeSelectionChangedListener {
 
-    private boolean finished = false;
+	private boolean finished = false;
 
-    private MockGeneratorWizardPage mainPage;
+	private MockGeneratorWizardPage mainPage;
 
-    private final IJavaElement classToMock;
+	private final IJavaElement classToMock;
 
-    private GroupMethodSelectionCtrl methodSelection;
+	private GroupMethodSelectionCtrl methodSelection;
 
-    private Vector<IMethod> checkedMethods;
+	private Vector<IMethod> checkedMethods;
 
-    private IJavaProject selectedProject = null;
+	private IJavaProject selectedProject = null;
 
-    private IPackageFragment targetPackage;
+	private IPackageFragment targetPackage;
 
-    private boolean isMockNew = true;
+	private boolean isMockNew = true;
 
-    private ICompilationUnit mockClass;
+	private ICompilationUnit mockClass;
 
-    public MockGeneratorWizard(IJavaElement javaElement) {
-	this.classToMock = javaElement;
-    }
-
-    @Override
-    public void addPages() {
-	String title = "Create mock-class";
-	mainPage = new MockGeneratorWizardPage(title);
-	mainPage.setTitle(title);
-
-	addPage(mainPage);
-    }
-
-    public void init() throws CoreException, JUTWarning {
-	// get mock project
-	IJavaProject project = JDTUtils.getProject(JUTPreferences
-		.getMockProject());
-	if (project == null) {
-	    project = JDTUtils.createProject(JUTPreferences.getMockProject(),
-		    classToMock.getJavaProject());
+	public MockGeneratorWizard(IJavaElement javaElement) {
+		this.classToMock = javaElement;
 	}
 
-	methodSelection = new GroupMethodSelectionCtrl();
-	methodSelection.init(mainPage.getView().getMethodSelectionGroup(),
-		classToMock);
-	methodSelection.addListener(this);
-	methodSelection.deactivateFilters();
+	@Override
+	public void addPages() {
+		String title = "Create mock-class";
+		mainPage = new MockGeneratorWizardPage(title);
+		mainPage.setTitle(title);
 
-	isMockNew = true;
-
-	if (project != null) {
-	    Text txtProject = mainPage.getView().getTxtProject();
-	    txtProject.setText(project.getElementName());
-	    txtProject.setData(project);
-
-	    this.selectedProject = project;
-
-	    // get target package
-	    IPackageFragment pckg = JDTUtils.getPackage(classToMock);
-	    this.targetPackage = JDTUtils.getPackage(project,
-		    pckg.getElementName() + ".mock", false);
-
-	    // get mock class
-	    if (targetPackage != null && targetPackage.exists()) {
-		String mockClassName = classToMock.getElementName().replace(
-			".java", "Mock.java");
-		mockClassName = mockClassName.replace(".class", "Mock.java");
-		if (!mockClassName.endsWith("Mock.java")) {
-		    mockClassName += "Mock.java";
-		}
-
-		mockClass = targetPackage.getCompilationUnit(mockClassName);
-
-		// get existing methods
-		if (mockClass != null && mockClass.exists()) {
-		    isMockNew = false;
-
-		    HashMap<MethodRef, IMethod> existingMethods = GeneratorUtils
-			    .getExistingTestMethods(mockClass);
-		    methodSelection.setExistingMethods(existingMethods);
-		}
-	    }
+		addPage(mainPage);
 	}
 
-	mainPage.getView().getBtnSelectProject()
-		.addSelectionListener(new SelectionAdapter() {
+	public void init() throws CoreException, JUTWarning {
 
-		    @Override
-		    public void widgetSelected(SelectionEvent e) {
-			super.widgetSelected(e);
-			handleContainer();
-		    }
-		});
-    }
+		// get mock project
+		IJavaProject project = JDTUtils.getProject(JUTPreferences
+				.getMockProject());
 
-    @Override
-    public boolean performFinish() {
-	finished = true;
-	return true;
-    }
-
-    @Override
-    public void init(IWorkbench arg0, IStructuredSelection arg1) {
-	// nothing
-    }
-
-    /**
-     * Returns if the processing is finished.
-     * 
-     * @return true if the processing is finished.
-     */
-    public boolean isFinished() {
-	return finished;
-    }
-
-    @Override
-    public void methodSelectionChanged(Vector<IMethod> chckdMethods) {
-	this.checkedMethods = chckdMethods;
-    }
-
-    public Vector<IMethod> getCheckedMethods() {
-	return checkedMethods;
-    }
-
-    private void handleContainer() {
-	List<IJavaProject> javaProjects = JDTUtils.getJavaProjects();
-	Set<IJavaProject> setProjects = new HashSet<IJavaProject>(javaProjects);
-
-	@SuppressWarnings("restriction")
-	ProjectSelectionDialog dialog = new ProjectSelectionDialog(getShell(),
-		setProjects);
-
-	if (dialog.open() == Window.OK) {
-	    Object[] results = dialog.getResult();
-	    if (results.length > 0) {
-		for (Object result : results) {
-		    if (result instanceof IJavaProject) {
-			IJavaProject project = ((IJavaProject) result);
-			mainPage.getView().getTxtProject()
-				.setText(project.getElementName());
-			mainPage.getView().getTxtProject().setData(result);
-			selectedProject = project;
-			return;
-		    }
+		if (project == null) {
+			project = JDTUtils.createProject(JUTPreferences.getMockProject(),
+					classToMock.getJavaProject());
 		}
-	    }
+
+		methodSelection = new GroupMethodSelectionCtrl();
+		methodSelection.init(mainPage.getView().getMethodSelectionGroup(),
+				classToMock);
+		methodSelection.addListener(this);
+		methodSelection.deactivateFilters();
+
+		isMockNew = true;
+
+		if (project != null) {
+			Text txtProject = mainPage.getView().getTxtProject();
+			txtProject.setText(project.getElementName());
+			txtProject.setData(project);
+
+			this.selectedProject = project;
+
+			// get target package
+			IPackageFragment pckg = JDTUtils.getPackage(classToMock);
+			this.targetPackage = JDTUtils.getPackage(project,
+					pckg.getElementName() + ".mock", false);
+
+			// get mock class
+			if (targetPackage != null && targetPackage.exists()) {
+				String mockClassName = classToMock.getElementName().replace(
+						".java", "Mock.java");
+				mockClassName = mockClassName.replace(".class", "Mock.java");
+				if (!mockClassName.endsWith("Mock.java")) {
+					mockClassName += "Mock.java";
+				}
+
+				mockClass = targetPackage.getCompilationUnit(mockClassName);
+
+				// get existing methods
+				if (mockClass != null && mockClass.exists()) {
+					isMockNew = false;
+
+					HashMap<MethodRef, IMethod> existingMethods = GeneratorUtils
+							.getExistingTestMethods(mockClass);
+					methodSelection.setExistingMethods(existingMethods);
+				}
+			}
+		}
+
+		mainPage.getView().getBtnSelectProject()
+				.addSelectionListener(new SelectionAdapter() {
+
+					@Override
+					public void widgetSelected(SelectionEvent e) {
+						super.widgetSelected(e);
+						handleContainer();
+					}
+				});
 	}
-    }
 
-    public IJavaProject getProject() {
-	return selectedProject;
-    }
+	@Override
+	public boolean performFinish() {
+		finished = true;
+		return true;
+	}
 
-    public IPackageFragment getTargetPackage() {
-	return targetPackage;
-    }
+	@Override
+	public void init(IWorkbench arg0, IStructuredSelection arg1) {
+		// nothing
+	}
 
-    public boolean isMockNew() {
-	return isMockNew;
-    }
+	/**
+	 * Returns if the processing is finished.
+	 * 
+	 * @return true if the processing is finished.
+	 */
+	public boolean isFinished() {
+		return finished;
+	}
 
-    public ICompilationUnit getMockClass() {
-	return mockClass;
-    }
+	@Override
+	public void methodSelectionChanged(Vector<IMethod> chckdMethods) {
+		this.checkedMethods = chckdMethods;
+	}
 
-    public HashMap<MethodRef, IMethod> getExistingMethods() {
-	return methodSelection.getExistingMethods();
-    }
+	public Vector<IMethod> getCheckedMethods() {
+		return checkedMethods;
+	}
 
-    /**
-     * {@inheritDoc}
-     * 
-     * @see org.junit.tools.ui.generator.wizards.IMethodeSelectionChangedListener#selectedMethodChecked(org.eclipse.jdt.core.IMethod)
-     */
-    @Override
-    public void selectedMethodChecked(IMethod selectedMethod) {
-	// nothing
-    }
+	private void handleContainer() {
+		List<IJavaProject> javaProjects = JDTUtils.getJavaProjects();
+		Set<IJavaProject> setProjects = new HashSet<IJavaProject>(javaProjects);
+
+		@SuppressWarnings("restriction")
+		ProjectSelectionDialog dialog = new ProjectSelectionDialog(getShell(),
+				setProjects);
+
+		if (dialog.open() == Window.OK) {
+			Object[] results = dialog.getResult();
+			if (results.length > 0) {
+				for (Object result : results) {
+					if (result instanceof IJavaProject) {
+						IJavaProject project = ((IJavaProject) result);
+						mainPage.getView().getTxtProject()
+								.setText(project.getElementName());
+						mainPage.getView().getTxtProject().setData(result);
+						selectedProject = project;
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	public IJavaProject getProject() {
+		return selectedProject;
+	}
+
+	public IPackageFragment getTargetPackage() {
+		return targetPackage;
+	}
+
+	public boolean isMockNew() {
+		return isMockNew;
+	}
+
+	public ICompilationUnit getMockClass() {
+		return mockClass;
+	}
+
+	public HashMap<MethodRef, IMethod> getExistingMethods() {
+		return methodSelection.getExistingMethods();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see org.junit.tools.ui.generator.wizards.IMethodeSelectionChangedListener#selectedMethodChecked(org.eclipse.jdt.core.IMethod)
+	 */
+	@Override
+	public void selectedMethodChecked(IMethod selectedMethod) {
+		// nothing
+	}
 }
